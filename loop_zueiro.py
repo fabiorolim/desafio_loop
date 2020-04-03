c8448e61f25c172e6bdb9369defca51812de9713
Fábio Rolim <fabiorolim@ifpi.edu.br>
2020-04-03 15:47:47 -0300

    Add files via upload

---
+++ loop_zueiro.py
@@ -0,0 +1,11 @@
+# TODO: Implemente uma função loop através de ou mais loops que retornem a saída
+#   coforme a lista expected.
+
+
+expected = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 5), (1, 6),
+            (1, 7), (1, 8), (1, 9), (2, 10), (2, 11), (2, 12), (2, 13),
+            (2, 14), (3, 15), (3, 16), (3, 17), (3, 18), (3, 19), (4, 20),
+            (4, 21), (4, 22), (4, 23), (4, 24), (5, 25), (5, 26), (5, 27),
+            (5, 28), (5, 29)]
+
+assert loop() == expected
